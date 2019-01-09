995252422b9e09d78b7cfb06138ae35bf9340bb0
Elias Hanna <h.elias@hotmail.fr>
2019-01-09 12:42:20 +0100

    Working transforms

--- src/rrt_path_finder_node.cpp
+++ src/rrt_path_finder_node.cpp
@@ -55,6 +55,7 @@ static void onMouse( int event, int x, int y, int, void* );
 
 
 
+
 Robot r;
 
 void odom_to_map(Robot& r, const Map& m)
@@ -62,9 +63,6 @@ void odom_to_map(Robot& r, const Map& m)
 	r.robot_pos_in_image[0] = r.robot_pos[0]/m.res - m.origin[0]/m.res;
 	r.robot_pos_in_image[1] = m.height - (r.robot_pos[1]/m.res - m.origin[1]/m.res);
 }
-
-
-
 
 static void onMouse( int event, int x, int y, int, void* )
 {
@@ -75,14 +73,6 @@ static void onMouse( int event, int x, int y, int, void* )
 
   std::cout << " x:" << x << "  y:" << y << std::endl;
 }
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-{
-	r.robot_pos[0] = msg->pose.pose.position.x;
-	r.robot_pos[1] = msg->pose.pose.position.y;
-}
-
-
 
 bool get_slam_map(Map& map)
 {
@@ -119,11 +109,13 @@ std::vector<Vertex*>& rrt(Vertex* v, double tX, double tY,cv::Mat &emptyMap)
 {
 	/* RRT Part */
 	cv::namedWindow( "Display window", cv::WINDOW_NORMAL );// Create a window for display.
-	//cv::namedWindow( "Display window2", cv::WINDOW_NORMAL );// Create a window for display.
+	cv::namedWindow( "Display window2", cv::WINDOW_NORMAL );// Create a window for display.
 	cv::setMouseCallback( "Display window", onMouse, 0 );
 	cv::Mat image,endIm;
+
 	emptyMap.copyTo(image);
-	emptyMap.copyTo(endIm);
+	cv::circle(image, cv::Point(v->data[0], v->data[1]), 10, cv::Scalar(0,255,0), -1, 8, 0);
+	image.copyTo(endIm);
 	
 	cv::Mat se = cv::getStructuringElement(cv::MORPH_ELLIPSE,cv::Size(10,10),cv::Point(-1,-1));
 	cv::erode(emptyMap, emptyMap, se, cv::Point(-1,-1), 1, cv::BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
@@ -133,7 +125,7 @@ std::vector<Vertex*>& rrt(Vertex* v, double tX, double tY,cv::Mat &emptyMap)
 	std::vector<Vertex*> vertices;
 	vertices.push_back(v);
 	
-	//vertices.push_back(new Vertex{{60.,60.},NULL,0,0});
+	// vertices.push_back(new Vertex{{60.,60.},NULL,0,0});
 	Vertex qrand, *qnear, *qnew=NULL;
 	int h = emptyMap.rows;
 	int w = emptyMap.cols;
@@ -143,7 +135,8 @@ std::vector<Vertex*>& rrt(Vertex* v, double tX, double tY,cv::Mat &emptyMap)
 		{
 			tX=targetX;
 			tY=targetY;
-			cv::circle(image, cv::Point(tX,tY), 10, cv::Scalar(255,0,0), -1, 8, 0);
+			cv::circle(image, cv::Point(tX,tY), 10, cv::Scalar(0,255,0), -1, 8, 0);
+
 			qnew = NULL;
 			Vertex target{{tX, tY}, NULL, 0.0, 0};
 			int ind = is_goal_reachable(target, vertices, emptyMap);
@@ -164,7 +157,7 @@ std::vector<Vertex*>& rrt(Vertex* v, double tX, double tY,cv::Mat &emptyMap)
 			vertices.push_back(qnew);
 			cv::line(image, vertex_to_point2f(*qnear), vertex_to_point2f(*qnew), cv::Scalar(0,0,255), 1, CV_AA);
 			cv::imshow( "Display window", image );                   // Show our image inside it.
-			//cv::imshow( "Display window2", emptyMap );  
+			cv::imshow( "Display window2", emptyMap );  
 			cv::waitKey(10);
 		}
 
@@ -178,31 +171,7 @@ std::vector<Vertex*>& rrt(Vertex* v, double tX, double tY,cv::Mat &emptyMap)
 
 	/* Shortening the path with straight lines part */
 	straighten_path(endIm, emptyMap, path);
-	 
-	/* Keep showing the map with the robot on it */
-	// while(ros::ok())
-	// 	{
-	// 		cv::imshow( "Display window", image );
-	// 		cv::waitKey(10);
-	// 	}
-	// /* Keep showing the map with the robot on it */
-	// while(ros::ok())
-	// 	{
-	// 		// flip(emptyMap, emptyMap, 0);
-	// 		//cv::circle(emptyMap, cv::Point(r.robot_pos_in_image[0], r.robot_pos_in_image[1]), ROBOT_RADIUS/map.res, cv::Scalar(255,255,255), 1, 8, 0);
-	// 		ros::spinOnce();
-	// 		//odom_to_map(r, map);
-	// 		// std::cout << r.robot_pos[0] << " " << r.robot_pos[1] << std::endl;
-	// 		// std::cout << r.robot_pos_in_image[0] << " " << r.robot_pos_in_image[1] << std::endl;
-	// 		//cv::circle(emptyMap, cv::Point(r.robot_pos_in_image[0], r.robot_pos_in_image[1]), ROBOT_RADIUS/map.res, cv::Scalar(0,0,0), 1, 8, 0);
-	// 		// flip(emptyMap, emptyMap, 0);
-	// 		cv::imshow( "Display window", emptyMap );                   // Show our image inside it. 
-	// 		//std::cout << cv::Point(r.robot_pos_in_image[0], r.robot_pos_in_image[1]) << std::endl;
-			       
-	// 		cv::waitKey(10);
-	  	
-	// 	}
-	
+	 	
 	std::vector<Vertex*>* pathCopy = new std::vector<Vertex*>(path); 
 	return *pathCopy;
 }
@@ -228,32 +197,29 @@ int main(int argc, char* argv[])
 {
 	bool without_mapping = false;
 	for(int i = 0 ; i < argc ; i++)
-		{
-			std::string arg(argv[i]);
-			if(arg == "--without-mapping")
-				without_mapping = true;
-		}
+	{
+		std::string arg(argv[i]);
+		if(arg == "--without-mapping")
+			without_mapping = true;
+	}
 
 	if(argc < 3 && without_mapping)
-		{
-			std::cout << "Usage: ./test [image_name] [flags: --without-mapping]" << std::endl;
-			return 1;
-		}
+	{
+		std::cout << "Usage: ./test [image_name] [flags: --without-mapping]" << std::endl;
+		return 1;
+	}
 	
 	ros::init(argc, argv, "rrt_path_finder_node");
 	if(without_mapping)
-		{
-			std::cout << "simple RRT exemple" << std::endl;
-			simpleRRT(argv[1]);
-			return 0;
-		}
+	{
+		std::cout << "simple RRT exemple" << std::endl;
+		simpleRRT(argv[1]);
+		return 0;
+	}
 
 	ros::NodeHandle n;
 	ros::Rate loop_rate(100); //10 Hz
 
-	// Subscribe to Odometry to get the position of the robot in the map
-	ros::Subscriber subOdom = n.subscribe("/odom", 10, odomCallback);
-	ros::spinOnce();
 	std::vector<Vertex*> vertices;
 	cv::Mat image,emptyMap, inMap;
 
@@ -268,67 +234,80 @@ int main(int argc, char* argv[])
 	ros::ServiceClient client = n.serviceClient<nav_msgs::GetMap>("dynamic_map");
 	nav_msgs::GetMap srv;
 	if (client.call(srv))
-		{
-			ROS_INFO("Service GetMap succeeded.");
-			std_msgs::Header header = srv.response.map.header;
-			nav_msgs::MapMetaData info = srv.response.map.info;
-			map.width = info.width;
-			map.height = info.height;
-			map.res = info.resolution;
-			map.origin[0] = info.origin.position.x;
-			map.origin[1] = info.origin.position.y;
-			map.data = &(srv.response.map.data[0]);
+	{
+		ROS_INFO("Service GetMap succeeded.");
+		std_msgs::Header header = srv.response.map.header;
+		nav_msgs::MapMetaData info = srv.response.map.info;
+		map.width = info.width;
+		map.height = info.height;
+		map.res = info.resolution;
+		map.origin[0] = info.origin.position.x;
+		map.origin[1] = info.origin.position.y;
+		map.data = &(srv.response.map.data[0]);
 
-			ROS_INFO("Map width, height: %u, %u", map.width, map.height);
-		}
+		ROS_INFO("Map width, height: %u, %u", map.width, map.height);
+	}
 	else
-		{
-			ROS_ERROR("Service GetMap failed.");
-			return 1;
-		}
+	{
+		ROS_ERROR("Service GetMap failed.");
+		return 1;
+	}
 	ROS_INFO("Map loading succeeded.");
 
 	if(map.height > 0 && map.width > 0)
-		{
-			emptyMap = cv::Mat(map.height, map.width, CV_8UC1, map.data);  
-	  
-			cv::Mat se = cv::getStructuringElement(cv::MORPH_ELLIPSE,cv::Size(ROBOT_RADIUS/map.res,ROBOT_RADIUS/map.res),cv::Point(-1,-1));
-			cv::dilate(emptyMap, emptyMap, se, cv::Point(-1,-1), 1, cv::BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	{
+		emptyMap = cv::Mat(map.height, map.width, CV_8UC1, map.data);  
+  
+		cv::Mat se = cv::getStructuringElement(cv::MORPH_ELLIPSE,cv::Size(ROBOT_RADIUS/map.res,ROBOT_RADIUS/map.res),cv::Point(-1,-1));
+		cv::dilate(emptyMap, emptyMap, se, cv::Point(-1,-1), 1, cv::BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 
-			cv::bitwise_not(emptyMap, emptyMap, cv::noArray());
+		cv::bitwise_not(emptyMap, emptyMap, cv::noArray());
 
-			cv::threshold(emptyMap, emptyMap, 254, 255, cv::THRESH_BINARY);
+		cv::threshold(emptyMap, emptyMap, 254, 255, cv::THRESH_BINARY);
 
-			flip(emptyMap, emptyMap, 0);
+		flip(emptyMap, emptyMap, 0);
 
-			ros::spinOnce();
-			odom_to_map(r, map);
-		}
+		odom_to_map(r, map);
+	}
 
 	targetX = r.robot_pos_in_image[0] + 1;
 	targetY = r.robot_pos_in_image[1];
 
-	// Get frame change between slam_karto map frame and the frame of the odom of the robot
-	tf::StampedTransform transform_slam;
-	ros::Time now = ros::Time::now();
-    if(t.waitForTransform("odom", "map", now, ros::Duration(3.0)))
-	    {
-	    	t.lookupTransform("odom", "map", ros::Time(0), transform_slam);
-	    	std::cout << transform_slam.getOrigin().x() << std::endl;
-	    }
-	// Get frame change between the frame of the odom of the robot and the frame representing the pose of the robot
-	tf::StampedTransform transform_pose;
-	now = ros::Time::now();
-    if(t.waitForTransform("base_footprint", "odom", now, ros::Duration(3.0)))
-	    {
-	    	t.lookupTransform("base_footprint", "odom", ros::Time(0), transform_pose);
-	    	std::cout << transform_pose.getOrigin().x() << std::endl;
-	    }
+	rrt(new Vertex{{81,81},NULL,0,0},targetX,targetY,emptyMap);
+	// rrt(new Vertex{{r.robot_pos_in_image[0],r.robot_pos_in_image[1]},NULL,0,0},targetX,targetY,emptyMap);
 
-	rrt(new Vertex{{24,254},NULL,0,0},targetX,targetY,emptyMap);
-	//rrt(new Vertex{{r.robot_pos_in_image[0],r.robot_pos_in_image[1]},NULL,0,0},targetX,targetY,emptyMap);
+	while(ros::ok())
+	{
+		// Get frame change between slam_karto map frame and the frame of the odom of the robot
+		tf::StampedTransform transform_slam;
+		try
+    	{
+      		t.lookupTransform("map", "base_footprint", ros::Time(0), transform_slam);
+    	}
+	    catch (tf::TransformException ex)
+    	{
+			ROS_ERROR("%s",ex.what());
+			ros::Duration(1.0).sleep();
+		}
+		r.robot_pos[0] = transform_slam.getOrigin().x();
+		r.robot_pos[1] = transform_slam.getOrigin().y();
 
-	while(ros::ok()){cv::waitKey(10);}
+		odom_to_map(r, map);
+		cv::circle(emptyMap, cv::Point(r.robot_pos_in_image[0], r.robot_pos_in_image[1]), ROBOT_RADIUS/map.res, cv::Scalar(0,0,0), -1, 8, 0);
+		// std::cout << std::endl;
+		// std::cout << r.robot_pos_in_image[0] << "|" << r.robot_pos_in_image[1] << std::endl;
+		// std::cout << transform_slam.getOrigin().x() << std::endl;
+	 //    std::cout << transform_slam.getOrigin().y() << std::endl;
+	 //    std::cout << transform_slam.getOrigin().z() << std::endl;
+	 //    std::cout << transform_slam.getRotation().x() << std::endl;
+	 //    std::cout << transform_slam.getRotation().y() << std::endl;
+	 //    std::cout << transform_slam.getRotation().z() << std::endl;
+	 //    std::cout << transform_slam.getRotation().w() << std::endl;  
+	    cv::imshow( "Display window", emptyMap );                   // Show our image inside it.
+		cv::circle(emptyMap, cv::Point(r.robot_pos_in_image[0], r.robot_pos_in_image[1]), ROBOT_RADIUS/map.res, cv::Scalar(255,0,0), -1, 8, 0);
+
+	    cv::waitKey(10);
+	}
 	
 	return 0;
 }
